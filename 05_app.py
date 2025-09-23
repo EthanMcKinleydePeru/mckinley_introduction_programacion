6918ebdf5ca78d73378df098a2f320e03c769abf
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-23 02:29:50 +0000

    Nuevo

---
+++ 05_app.py
@@ -0,0 +1,7 @@
+#obtener el promedio de un estudiante a partir de 3 notas
+
+#Inicio variables de las notas
+
+#Proceso el procesp de calculo del promedio de notas
+
+#Salida mostrar el resultado del promedio
