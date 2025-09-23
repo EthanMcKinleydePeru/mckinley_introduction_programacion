b84c909a2b0060c28d273ede9ab0257e7060b95e
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-23 01:25:21 +0000

    si

---
+++ 03_app.py
@@ -0,0 +1,15 @@
+#Variables
+
+#Los variables son palabras especificas que utilizamos para asignar y almacenar valores especificos que podemos recuperar en cualquier momento.
+
+#Variable nombre
+nombre = "Ethan"
+
+telefono = 997654111
+
+name = "Alberto"
+
+#Visualizando el contenido de las variables
+print(nombre)
+print(telefono)
+print(name)
