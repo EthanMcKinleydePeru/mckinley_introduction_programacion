481b9dd4ea25157773685530ecd67559559425ed
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-26 03:33:15 +0000

    calculo de edad ejercicio

---
+++ 08_app.py
@@ -0,0 +1,10 @@
+#Calcular la edad de una persona a partir de su ano de nacimiento
+
+#Defini una variable que reciba el numero de ano de nacimiento de la persona
+year_birthday = input("Ingresa el ano de tu nacimiento: ")
+
+#Operacion de calculo de edad: ano_actual - ano_nacimiento
+edad = 2025 - int(year_birthday)
+
+#Visualiza la edad
+print(f"La edad de la persona es: {edad}")
