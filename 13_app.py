0c9314c2b89b9a6b42b370ea42591d8d836a5174
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-30 01:39:15 +0000

    practicar con elif

---
+++ 13_app.py
@@ -0,0 +1,12 @@
+# Sintaxis
+# if condicion:
+#    instucciones a ejecutarse si la condicion es verdadera
+# else
+#   instrucciones a ejecutar si la condicion es falsa
+
+nota = 17
+
+if nota >=13:
+    print("Usted ha aprobado el curso")
+else:
+    print("Usted no ha aprobado el curso")
---
+++ 14_app.py
@@ -0,0 +1,22 @@
+# if condicion:
+#    instucciones a ejecutarse si la condicion es verdadera
+# elif otra condicion:
+#   instrucciones para la segunda condicion
+# else:
+#   instrucciones a ejecutar si la condicion es falsa
+
+
+#nota0-12.4: desaprobado
+#nota 12.5-16: Aprobado
+#nots17-20: Muy bien, sigue asi
+
+
+nota = 17
+
+if nota <=12.4:
+    print("Usted ha desprobado el curso")
+elif nota<17:
+    print ("Usted ha aprobado el curso") 
+else:
+    print("Muy bien, sigue asi!")
+    
