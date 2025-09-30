51b1ec2934867c748f2c27aedad302789286875c
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-30 02:31:40 +0000

    trabaja con for

---
+++ 16_app.py
@@ -0,0 +1,13 @@
+# EStructura de bucle For
+
+# Sintaxis
+# for value in sequence:
+#   instruccion, que es lo que debe hacer mientras se cumple el bucle
+
+#Ejemplo
+
+#Lista de frutas
+frutas = ["fresa", "mango", "platano", "manzana", "uva", "maracuya"]
+
+for nombre in frutas:
+    print(nombre)
