4dce8bac8aa76e6f5a1374db4bef536aedf5ddfa
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-30 00:46:04 +0000

    primer ejemplo con if

--- 09_app.py
+++ 09_app.py
@@ -1,14 +1,13 @@
-#Calculo de monto a pagar para una factura (IGV - 18%)
+# Calculo de monto a pagar para una factura (IGV - 18%)
 
-#Definir el subtotal
+# Definir el subtotal
 subtotal = float(input("Ingresa el subtotal: "))
 
-
-#Definir el calculo
+# Definir el calculo
 monto_igv = subtotal * .18
 monto_pagar = subtotal + monto_igv
 
-#Imprimir resultados
+# Imprimir resultados
 print(f"Monto de subtotal : {subtotal}")
 print(f"Monto de IGV 18%: {monto_igv}")
 print(f"Monto total a pagar: {monto_pagar}")
---
+++ 10_app.py
@@ -0,0 +1,15 @@
+#Estructura condicional If
+
+#sintaxis basica
+#  if condicion:
+#    instucciones a ejecutarse
+
+#Ejemplo
+
+#Definir una variable cantidad
+cantidad = 3
+
+# Hacer comparacion con if
+# La variable cantidad es mayor o igual a 5
+if cantidad >= 5:
+    print("si")
