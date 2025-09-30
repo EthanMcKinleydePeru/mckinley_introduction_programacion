261432db105f981975d29650d9457d2a14d7336a
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-09-30 01:01:04 +0000

    if con else

---
+++ 12_app.py
@@ -0,0 +1,15 @@
+#Estructura condicional If - else
+
+# Sintaxis
+# if condicion:
+#    instucciones a ejecutarse si la condicion es verdadera
+# else
+#   instrucciones a ejecutar si la condicion es falsa
+
+#Ejemplo
+edad = 25
+
+if edad < 18:
+    print("La persona es menor de edad")
+else:
+    print("La persona es mayor o igual que edad")
