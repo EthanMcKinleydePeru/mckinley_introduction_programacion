062c3905eafcb0265a9f601ce0afff6cb180381d
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-10-07 02:38:52 +0000

    cambios

--- caso_practico.py
+++ caso_practico.py
@@ -16,12 +16,12 @@ impuestos_a_la_renta = total_por_impuestos * .08
 sueldo_final = sueldo_base + comision + bono - impuestos_a_la_renta
 
 print(f"--- Reporte para {nombre} ---")
-print(f"Sueldo Base      : S/ {sueldo_base}")
-print(f"Ventas del Mes   : S/ {ventas}")
+print(f"Sueldo Base      : S/ {sueldo_base:.2f}")
+print(f"Ventas del Mes   : S/ {ventas:.2f}")
 print("--------------------------------")
-print(f"+ Comision  (8%)   : S/ {comision}")
-print(f"+ Bono             : S/ {bono}")
-print(f"- Impuesto  (8%)   : S/ {impuestos_a_la_renta}")
+print(f"+ Comision  (8%)   : S/ {comision:.2f}")
+print(f"+ Bono             : S/ {bono:.2f}")
+print(f"- Impuesto  (8%)   : S/ {impuestos_a_la_renta:.2f}")
 print("--------------------------------")
-print(f"Sueldo Final Neto  : S/ {sueldo_final}")
+print(f"Sueldo Final Neto  : S/ {sueldo_final:.2f}")
 print("================================")
