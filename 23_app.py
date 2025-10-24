288375068c3ee7d7063140848735dd33e5c053b7
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-10-24 03:29:24 +0000

    listas

---
+++ 23_app.py
@@ -0,0 +1,7 @@
+dias = ["lunes","martes","miercoles", "jueves", "viernes"]
+for a in dias:
+    print(a)
+
+print(dias[2])
+
+print(dias[len(dias)-1])
---
+++ 24_app.py
@@ -0,0 +1,4 @@
+notas = [10,7,15,13,6,10]
+
+print(notas)
+print(notas[-1])
