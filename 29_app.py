e1d69b6e08bcc2b3bb40ebee307efae87c2a2be1
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-10-28 01:19:31 +0000

    si

---
+++ 29_app.py
@@ -0,0 +1,16 @@
+# Listas
+
+# Tenemos áreas de una organización
+areas = ["Administración", "Recursos Humanos", "Contabilidad", "Sistemas"]
+
+# Buscar elementos dentro de lista
+print("Recursos Humanos" in areas)
+
+# Buscar elementos dentro de lista
+print("Recursos humanos" in areas)
+
+# Buscar contabilidad
+print("contabilidad" in areas)
+
+# Buscar Sistemas
+print("Sistemas" in areas)
