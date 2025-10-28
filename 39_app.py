958938ec544cf163465da38e77908d7618a682cf
EthanMcKinleydePeru <i202519290@cibertec.edu.pe>
2025-10-28 03:02:59 +0000

    si

--- 39_app.py
+++ 39_app.py
@@ -7,3 +7,6 @@ listaPaises.append(input("Ingresa el nombre de un pais "))
 listaPaises.append(input("Ingresa el nombre de un pais "))
 
 print(listaPaises)
+
+for pais in listaPaises:
+    print(pais)
